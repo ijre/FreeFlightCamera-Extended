094bb23054a70aff4236822ee477608c5fe3dff4
Paige Seivertson <pzeig66@yahoo.com>
2022-02-07 04:37:40 -0500

    Game Speed now resets when exiting missions

--- Free Flight Camera Extended/Extensions/main.lua
+++ Free Flight Camera Extended/Extensions/main.lua
@@ -1,6 +1,16 @@
 local Ext = FFC.Ext or { }
 local Paths = Ext.Paths
 
+if RequiredScript == "lib/managers/statisticsmanager" then
+  local origStopSesh = StatisticsManager.stop_session
+  function StatisticsManager:stop_session(data)
+    FFC:set_game_speed(1.0)
+    origStopSesh(self, data)
+  end
+
+  return
+end
+
 dofile(Paths.Ext .. "GUI.lua")
 dofile(Paths.Keybinds .. "main.lua")
 
